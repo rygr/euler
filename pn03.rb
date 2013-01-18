715940cf1bb7bdd3018752f3be512e26d85758c7
Ryan Grablick <rgrablick@gmail.com>
2013-01-18 11:49:12 -0500

    prime factors description

---
+++ pn03.rb
@@ -0,0 +1,9 @@
+# Test Scenario:
+#   Given 13195
+#   Then prime factors are 5, 7, 13 and 9
+#   And the greatest prime factor is 29
+
+# Actual Scenario:
+#   Given 600851475143
+#   Then prime factors are ????
+#   And the greatest prime factor is ???
